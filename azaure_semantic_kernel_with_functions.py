c9fce77464b7f9a8d2a6ba901decbda56fc89f69
Zachary Vu Nguyen <111367069+nguyends@users.noreply.github.com>
2024-08-30 11:56:44 -0500

    Finished azaure_semantic_kernel_with_functions.py

---
+++ azaure_semantic_kernel_with_functions.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os
+from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.prompt_template import PromptTemplateConfig
+import asyncio
+
+
+async def main():
+    load_dotenv()
+    deployment_name = os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"]
+    endpoint = os.environ["AZURE_OPENAI_ENDPOINT"]
+    api_key = os.environ["AZURE_OPENAI_API_KEY"]
+
+    kernel = Kernel()
+    kernel.add_service(AzureChatCompletion(
+            deployment_name=deployment_name,
+            endpoint=endpoint,
+            api_key=api_key,
+        ))
+
+    script_directory = os.path.dirname(__file__)
+    plugins_directory = os.path.join(script_directory, "plugins")
+    writer_plugin = kernel.add_plugin(parent_directory=plugins_directory, plugin_name="WinePlugin")
+
+    result = await kernel.invoke(writer_plugin["Somellier"], input="Merlot")
+
+    print(result)
+
+# Run the main function
+if __name__ == "__main__":
+    asyncio.run(main())
